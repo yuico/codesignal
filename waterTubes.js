a8d5e7a4091d5c97e03edc2daa854b262917ec84
yui <nokdoo00@gmail.com>
2019-09-29 19:08:19 -0400

    add

---
+++ waterTubes.js
@@ -0,0 +1,12 @@
+function waterTubes(water, flowPerMinute) {
+    let time = 0, temp=0, i=0;
+    for(; i<water.length; i++) {
+        temp = Math.ceil(water[i]/flowPerMinute[i]);
+        if(time < temp) {
+            time = temp;
+        }
+    }return time;
+}
+console.log(waterTubes(water, flowPerMinute));
+let water = [1, 2, 5];
+let flowPerMinut = [1, 1, 2];
